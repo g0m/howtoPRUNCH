29d04f03ea53211f1dbd1b93eca9ddb4aef5a3d8
g0m <g0mmyg0m@gmail.com>
2026-09-11 03:39:22 +0100

    added a hyperlink to grunch
    Updated the description for 'grunch' to include a link.

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ var myArray = [
 ["troubled genius", "To Prunch, add a target card from your sideboard to your hand. Its colored mana pips perpetually become twobrid.<br>'?' is equal to its new MV."],
 ["chubby funster", "To Prunch, exile any number of cards from your hand, and then add that many plus one cards from your sideboard to your hand.<br>'?' is equal to the number of cards exiled by this effect."],
 ["UB staple", "To Prunch, add an additional beginning phase at the start of your next end phase.<br>'?' becomes equal to the number of phases you took this turn at the end of each end phase."],
-["grunch", "To Prunch, Grunch.<br>'?' is equal to whatever '?' is equal to."],
+["grunch", "To Prunch, <a href=\"https://tinyurl.com/howtoGRUNCH\">Grunch</a>.<br>'?' is equal to whatever '?' is equal to."],
 ["purple man (not the marvel one)", "To Prunch, you may draft a card into your hand from the battlefield.<br>'?' is equal to its MV."],
 ["noun", "To Prunch, you may put a purple permanent from your graveyard onto the battlefield.<br>'?' is equal to your devotion to purple."],
 ["homunculus", "To Prunch, destroy up to two target conspiracies and/or emblems.<br>'?' is equal to 3."],
